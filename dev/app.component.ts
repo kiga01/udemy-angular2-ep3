7cc1bd6649f05aae0aa07daf23d77fa43d431839
Alejandro Hinestrosa <ahinestrosam@psl.com.co>
2016-04-06 15:57:48 -0500

    initial config

---
+++ dev/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <div class="container-fluid">
+            <h1>hello world!</h1>
+        </div>
+    `,
+})
+export class AppComponent {
+
+}
